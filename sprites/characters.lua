29b82ebc81f940f616dce27307add512ba71778b
蕭志軒 <keviner2004@gmail.com>
2016-11-19 18:56:22 +0800

    Add characters sprites

---
+++ sprites/characters.lua
@@ -0,0 +1,531 @@
+--
+-- created with TexturePacker (http://www.codeandweb.com/texturepacker)
+--
+-- $TexturePacker:SmartUpdate:a38cc4074e4d43969dbc7125d39e198f:a33f902e744b36e39672bb09961fcb91:f94c4674a58e1ee53e1cb2fa4af30ee6$
+--
+-- local sheetInfo = require("mysheet")
+-- local myImageSheet = graphics.newImageSheet( "mysheet.png", sheetInfo:getSheet() )
+-- local sprite = display.newSprite( myImageSheet , {frames={sheetInfo:getFrameIndex("sprite")}} )
+--
+
+local SheetInfo = {}
+
+SheetInfo.sheet =
+{
+    frames = {
+    
+        {
+            -- 1/1
+            x=1,
+            y=248,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 1/2
+            x=69,
+            y=253,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 1/3
+            x=349,
+            y=362,
+            width=68,
+            height=94,
+
+        },
+        {
+            -- 1/4
+            x=72,
+            y=1,
+            width=70,
+            height=72,
+
+        },
+        {
+            -- 1/5
+            x=137,
+            y=253,
+            width=70,
+            height=93,
+
+        },
+        {
+            -- 1/6
+            x=419,
+            y=362,
+            width=68,
+            height=94,
+
+        },
+        {
+            -- 1/7
+            x=278,
+            y=554,
+            width=70,
+            height=96,
+
+        },
+        {
+            -- 1/8
+            x=209,
+            y=255,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 1/9
+            x=277,
+            y=255,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 1/10
+            x=207,
+            y=552,
+            width=69,
+            height=96,
+
+        },
+        {
+            -- 1/11
+            x=73,
+            y=637,
+            width=70,
+            height=97,
+
+        },
+        {
+            -- 2/1
+            x=345,
+            y=266,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 2/2
+            x=413,
+            y=267,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 2/3
+            x=1,
+            y=438,
+            width=66,
+            height=94,
+
+        },
+        {
+            -- 2/4
+            x=144,
+            y=1,
+            width=68,
+            height=72,
+
+        },
+        {
+            -- 2/5
+            x=69,
+            y=348,
+            width=68,
+            height=93,
+
+        },
+        {
+            -- 2/6
+            x=69,
+            y=443,
+            width=66,
+            height=94,
+
+        },
+        {
+            -- 2/7
+            x=137,
+            y=539,
+            width=68,
+            height=96,
+
+        },
+        {
+            -- 2/8
+            x=1,
+            y=343,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 2/9
+            x=139,
+            y=348,
+            width=66,
+            height=93,
+
+        },
+        {
+            -- 2/10
+            x=350,
+            y=555,
+            width=69,
+            height=96,
+
+        },
+        {
+            -- 2/11
+            x=145,
+            y=650,
+            width=70,
+            height=97,
+
+        },
+        {
+            -- 3/1
+            x=73,
+            y=159,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 3/2
+            x=141,
+            y=159,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 3/3
+            x=137,
+            y=443,
+            width=68,
+            height=94,
+
+        },
+        {
+            -- 3/4
+            x=214,
+            y=1,
+            width=70,
+            height=72,
+
+        },
+        {
+            -- 3/5
+            x=207,
+            y=350,
+            width=70,
+            height=93,
+
+        },
+        {
+            -- 3/6
+            x=207,
+            y=445,
+            width=68,
+            height=94,
+
+        },
+        {
+            -- 3/7
+            x=421,
+            y=555,
+            width=70,
+            height=96,
+
+        },
+        {
+            -- 3/8
+            x=209,
+            y=161,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 3/9
+            x=277,
+            y=161,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 3/10
+            x=415,
+            y=458,
+            width=69,
+            height=95,
+
+        },
+        {
+            -- 3/11
+            x=217,
+            y=652,
+            width=70,
+            height=97,
+
+        },
+        {
+            -- 4/1
+            x=356,
+            y=1,
+            width=66,
+            height=82,
+
+        },
+        {
+            -- 4/2
+            x=424,
+            y=1,
+            width=66,
+            height=82,
+
+        },
+        {
+            -- 4/3
+            x=209,
+            y=75,
+            width=68,
+            height=84,
+
+        },
+        {
+            -- 4/4
+            x=1,
+            y=1,
+            width=69,
+            height=68,
+
+        },
+        {
+            -- 4/5
+            x=69,
+            y=75,
+            width=70,
+            height=82,
+
+        },
+        {
+            -- 4/6
+            x=279,
+            y=75,
+            width=68,
+            height=84,
+
+        },
+        {
+            -- 4/7
+            x=420,
+            y=85,
+            width=70,
+            height=86,
+
+        },
+        {
+            -- 4/8
+            x=1,
+            y=71,
+            width=66,
+            height=82,
+
+        },
+        {
+            -- 4/9
+            x=141,
+            y=75,
+            width=66,
+            height=82,
+
+        },
+        {
+            -- 4/10
+            x=349,
+            y=85,
+            width=69,
+            height=85,
+
+        },
+        {
+            -- 4/11
+            x=1,
+            y=159,
+            width=70,
+            height=87,
+
+        },
+        {
+            -- 5/1
+            x=345,
+            y=172,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 5/2
+            x=413,
+            y=173,
+            width=66,
+            height=92,
+
+        },
+        {
+            -- 5/3
+            x=277,
+            y=456,
+            width=66,
+            height=94,
+
+        },
+        {
+            -- 5/4
+            x=286,
+            y=1,
+            width=68,
+            height=72,
+
+        },
+        {
+            -- 5/5
+            x=279,
+            y=361,
+            width=68,
+            height=93,
+
+        },
+        {
+            -- 5/6
+            x=345,
+            y=458,
+            width=68,
+            height=94,
+
+        },
+        {
+            -- 5/7
+            x=1,
+            y=636,
+            width=70,
+            height=96,
+
+        },
+        {
+            -- 5/8
+            x=1,
+            y=534,
+            width=66,
+            height=95,
+
+        },
+        {
+            -- 5/9
+            x=69,
+            y=539,
+            width=66,
+            height=95,
+
+        },
+        {
+            -- 5/10
+            x=289,
+            y=653,
+            width=69,
+            height=98,
+
+        },
+        {
+            -- 5/11
+            x=360,
+            y=653,
+            width=70,
+            height=100,
+
+        },
+    },
+    
+    sheetContentWidth = 492,
+    sheetContentHeight = 754
+}
+
+SheetInfo.frameIndex =
+{
+
+    ["1/1"] = 1,
+    ["1/2"] = 2,
+    ["1/3"] = 3,
+    ["1/4"] = 4,
+    ["1/5"] = 5,
+    ["1/6"] = 6,
+    ["1/7"] = 7,
+    ["1/8"] = 8,
+    ["1/9"] = 9,
+    ["1/10"] = 10,
+    ["1/11"] = 11,
+    ["2/1"] = 12,
+    ["2/2"] = 13,
+    ["2/3"] = 14,
+    ["2/4"] = 15,
+    ["2/5"] = 16,
+    ["2/6"] = 17,
+    ["2/7"] = 18,
+    ["2/8"] = 19,
+    ["2/9"] = 20,
+    ["2/10"] = 21,
+    ["2/11"] = 22,
+    ["3/1"] = 23,
+    ["3/2"] = 24,
+    ["3/3"] = 25,
+    ["3/4"] = 26,
+    ["3/5"] = 27,
+    ["3/6"] = 28,
+    ["3/7"] = 29,
+    ["3/8"] = 30,
+    ["3/9"] = 31,
+    ["3/10"] = 32,
+    ["3/11"] = 33,
+    ["4/1"] = 34,
+    ["4/2"] = 35,
+    ["4/3"] = 36,
+    ["4/4"] = 37,
+    ["4/5"] = 38,
+    ["4/6"] = 39,
+    ["4/7"] = 40,
+    ["4/8"] = 41,
+    ["4/9"] = 42,
+    ["4/10"] = 43,
+    ["4/11"] = 44,
+    ["5/1"] = 45,
+    ["5/2"] = 46,
+    ["5/3"] = 47,
+    ["5/4"] = 48,
+    ["5/5"] = 49,
+    ["5/6"] = 50,
+    ["5/7"] = 51,
+    ["5/8"] = 52,
+    ["5/9"] = 53,
+    ["5/10"] = 54,
+    ["5/11"] = 55,
+}
+
+function SheetInfo:getSheet()
+    return self.sheet;
+end
+
+function SheetInfo:getFrameIndex(name)
+    return self.frameIndex[name];
+end
+
+return SheetInfo
