884e770b325a85bcab61db7922cdee6743e01364
onlyeric23 <onlyeric23@gmail.com>
2016-12-19 19:05:04 +0800

    sketches of save and load

--- scenes/Editor/StateManager.lua
+++ scenes/Editor/StateManager.lua
@@ -1,18 +1,19 @@
 local sqlite3 = require("sqlite3")
+local lfs = require("lfs")
 
 local manager = {}
+
+local basePath = system.pathForFile("", system.DocumentsDirectory)
 
 local create_tables = [=[
 	CREATE TABLE IF NOT EXISTS worlds(
 		id INTEGER PRIMARY KEY,
-		culture TEXT,
-		save_date DATETIME
+		culture TEXT
 	);
 	CREATE TABLE IF NOT EXISTS layers(
 		id INTEGER PRIMARY KEY,
 		type TEXT,
-		world_id INTEGER,
-		save_date DATETIME
+		world_id INTEGER
 	);
 	CREATE TABLE IF NOT EXISTS tiles(
 		id INTEGER PRIMARY KEY,
@@ -20,33 +21,67 @@ local create_tables = [=[
 		name TEXT,
 		x INTEGER,
 		y INTEGER,
-		layer_id INTEGER,
-		save_date DATETIME
+		layer_id INTEGER
 	);
 ]=]
 
 local manager = {}
-manager.db = nil
 
-function manager:save()
-		
+function manager:save(file)
+	local db = sqlite3.open(file)
+	-- save
+	db:close()
+end
+
+function manager:saveOld()
+	-- first save file
+	if self.lastSaveIdx == -1 then
+		print("save not found")
+		self:saveNew()
+		return
+	end
+	-- last save file
+	local oldFile = tostring(self.lastSaveIdx)..".save"
+	self.save(oldFile)
+end
+
+function manager:saveNew()
+	self.lastSaveIdx = self.lastSaveIdx + 1
+	local newFile = tostring(self.lastSaveIdx)..".save"
+	self.save(newFile)
 end
 
 function manager:load(file)
-	
+	local db = sqlite3.open(file)
+	-- load
+	db:close()
 end
 
 function manager:loadLast()
-	
+	if self.lastSaveIdx == -1 then
+		print("save not found")
+		return
+	end
+	self.load(tostring(self.lastSaveIdx)..".save")
 end
 
 function manager:initial(universe)
-	self.db = sqlite3.open("state.db")
+	-- find max save
+	local max = -1
+	for fileName in lfs.dir(basePath) do
+		-- get save idx
+		local saveIdx = tonumber( string.match(fileName, "%d+.save") )
+		print("save idx", saveIdx)
+		if max < saveIdx then
+			max = saveIdx
+		end
+	end
+	self.lastSaveIdx = max
+
 	self.universe = universe
 end
 
 function manager:destroy()
-	self.db:close()
 	self.universe = nil
 end
 
